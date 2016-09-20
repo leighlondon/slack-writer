eb6524fccf063b7583adccd732f7ad693966e1c0
Leigh London <leigh.00.london@gmail.com>
2016-09-20 14:55:03 +1000

    Basic fields test.

--- writer_test.go
+++ writer_test.go
@@ -29,3 +29,31 @@ func TestNewWriter(t *testing.T) {
 		}
 	}
 }
+
+func TestWriterFields(t *testing.T) {
+	samples := []struct {
+		channel string
+		user    string
+		token   string
+	}{
+		{"#channelname", "username", "token"},
+	}
+
+	for _, s := range samples {
+		w, err := NewWriter(s.channel, s.user, s.token)
+		if err != nil {
+			t.Errorf("NewWriter failed c=%s u=%s t=%s",
+				s.channel, s.user, s.token)
+		}
+		if c := w.Channel(); c != s.channel {
+			t.Errorf(".Channel(): expected %s got %s", s.channel, c)
+		}
+		if u := w.Username(); u != s.user {
+			t.Errorf(".Username(): expected %s got %s", s.user, u)
+		}
+		if a := w.Token(); a != s.token {
+			t.Errorf(".Token(): expected %s got %s", s.token, a)
+		}
+
+	}
+}
