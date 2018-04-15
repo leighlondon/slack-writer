2aa9ea5788cfb9e365d427527c2ab60da54bf37e
Leigh London <leigh.00.london@gmail.com>
2018-04-15 16:35:18 +1000

    doc: split out

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package slack provides a writer that implements the io.Writer interface,
+// meaning that it can be used to dump messages to a Slack channel directly
+// from Go code.
+package slack
--- writer.go
+++ writer.go
@@ -1,6 +1,3 @@
-// Package slack provides a writer that implements the io.Writer interface,
-// meaning that it can be used to dump messages to a Slack channel directly
-// from Go code.
 package slack
 
 import (
